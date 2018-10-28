4711878fb99375e0f8a3f50bc8b24c38771270e0
pokeflutist78770 <a.aguayo30@gmail.com>
2018-10-27 18:32:17 -0700

    New Sword Velocity added

--- Sword.cs
+++ Sword.cs
@@ -14,6 +14,10 @@ public class Sword : MonoBehaviour {
 		
 	}
 
+	private void FixedUpdate() {
+		print(transform.root.gameObject.GetComponent<Rigidbody>().velocity);
+	}
+
 	private void OnTriggerEnter(Collider other) {
 		print("Sword Collision");
 		//Destroy(other.gameObject);
@@ -22,8 +26,43 @@ public class Sword : MonoBehaviour {
 			print("Enemy Collided");
 			other.transform.root.gameObject.GetComponent<Animator>().enabled = false;
 		}
-		else if(other.transform.root.gameObject.CompareTag("Wall")){
-			this.transform.root.gameObject.GetComponent<PlayerController>().health-=5;
+		else if (other.gameObject.CompareTag("Wall")) {
+			other.transform.root.gameObject.GetComponent<PlayerController>().health -= 5;
+
 		}
+
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		print("Sword Collision");
+		//Destroy(other.gameObject);
+		if (other.transform.root.gameObject.CompareTag("Enemy")) {
+
+			print("Enemy Collided");
+			other.transform.root.gameObject.GetComponent<Animator>().enabled = false;
+
+			Vector3 velocity = this.transform.root.gameObject.GetComponent<Rigidbody>().velocity;
+
+			other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3((velocity.x*5), 10, (velocity.z*5)), ForceMode.Impulse);
+
+			//this.transform.root.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 0), ForceMode.VelocityChange);
+			/*
+			Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+			foreach (Collider hit in colliders) {
+				print(hit.gameObject.tag);
+				
+				Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+				if (rb != null && rb.gameObject.tag!="Player")
+					rb.AddExplosionForce(power, explosionPos, radius, 200.0F);
+				
+			}
+			*/
+		}
+		else if (other.gameObject.CompareTag("Wall")) {
+			this.transform.root.gameObject.GetComponent<PlayerController>().health -= 5;
+
+		}
+
 	}
 }
