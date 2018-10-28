606b1cfe08b9bef4c533f46859cfa290e51c2009
pokeflutist78770 <a.aguayo30@gmail.com>
2018-10-27 21:17:27 -0700

    WOOOO

---
+++ Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+	public int speed;
+	public bool isAlive;
+	public AudioClip death;
+
+	// Use this for initialization
+	void Start () {
+		speed = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAlive) {
+			//transform.Rotate(new Vector3(speed * 15 * Time.deltaTime, speed * 30 * Time.deltaTime, speed * 45 * Time.deltaTime));
+		}
+      
+	}
+
+}
